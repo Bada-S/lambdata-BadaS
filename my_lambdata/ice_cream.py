6ad335b1f5ead7ac07d214d2826e481261009606
Bada-S <jamessopkin@gmail.com>
2020-10-01 05:32:09 -0500

    pep8

--- my_lambdata/ice_cream.py
+++ my_lambdata/ice_cream.py
@@ -1,14 +1,14 @@
-
 class Icecream(object):
     def __init__(self, flavor, style, container, toppings="None"):
         self.flavor = flavor
         self.style = style
         self.container = container
         self.toppings = toppings
+
     def serve(self):
         print("ENJOY!")
 if __name__ == "__main__":
     icecream = Icecream("Chocolate", "Scoop", "Cone", "Almonds")
     print(icecream.flavor, icecream.style, icecream.container,
-     icecream.toppings)
+          icecream.toppings)
     icecream.serve()
